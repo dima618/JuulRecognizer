fa61f8ae823d1b3988bacceabefa5224ff7c2f7c
Dima Ilin <dimailinemail@gmail.com>
2019-11-17 15:44:27 -0500

    Add project files.

---
+++ JuulRecognizer/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace JuulRecognizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoFrame = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.videoFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoFrame
+            // 
+            this.videoFrame.Dock = System.Windows.Forms.DockStyle.Top;
+            this.videoFrame.Location = new System.Drawing.Point(0, 0);
+            this.videoFrame.Name = "videoFrame";
+            this.videoFrame.Size = new System.Drawing.Size(1245, 499);
+            this.videoFrame.TabIndex = 2;
+            this.videoFrame.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 664);
+            this.Controls.Add(this.videoFrame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoFrame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox videoFrame;
+    }
+}
+
